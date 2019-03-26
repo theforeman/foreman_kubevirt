3f954ed311bddc853de5200be907fa7094ed2a76
Moti Asayag <masayag@redhat.com>
2019-03-26 11:46:58 +0200

    Support Storage Class when creating VM

--- app/assets/javascripts/compute_resources/kubevirt/volume.js
+++ app/assets/javascripts/compute_resources/kubevirt/volume.js
@@ -43,3 +43,14 @@ setPvcSize = function (pvc, pvcSizeMod) {
     pvcSizeMod.val(null);
   }
 }
+
+bootableRadio = function (item) {
+  const disabled = $('[id$=_bootable_true]:disabled:checked:visible');
+
+  $('[id$=_bootable_true]').prop('checked', false);
+  if (disabled.length > 0) {
+    disabled.prop('checked', true);
+  } else {
+    $(item).prop('checked', true);
+  }
+}
