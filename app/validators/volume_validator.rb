5f1e281414d0b13e7730928e5f9513d913ce07ac
Shira Maximov <shiramaximov@gmail.com>
2019-09-24 12:28:29 +0300

    Fixes #27929 - Fix capacity validation to work only on kubevirt CR

--- app/validators/volume_validator.rb
+++ app/validators/volume_validator.rb
@@ -1,8 +1,12 @@
 class VolumeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    value[:volumes_attributes].each do |_, attrs|
-      if attrs["capacity"].to_s.empty? || /\A\d+G?\Z/.match(attrs["capacity"].to_s).nil?
-        record.errors.add(attribute, _("Volume size #{attrs["capacity"]} is not valid"))
+    if value[:volumes_attributes].present?
+      value[:volumes_attributes].each do |_, attrs|
+        if attrs.key?("capacity") and attrs.key?("storage_class")
+          if attrs["capacity"].to_s.empty? || /\A\d+G?\Z/.match(attrs["capacity"].to_s).nil?
+            record.errors.add(attribute, _("Volume size #{attrs["capacity"]} is not valid"))
+          end
+        end
       end
     end
   end
