7a0bdb424ea28c955879311ed8cc0177e1d091b2
Michael Moll <kvedulv@kvedulv.de>
2019-12-14 14:04:00 +0100

    Fix Rubocop

--- app/controllers/foreman_kubevirt/concerns/api/compute_resources_controller_extensions.rb
+++ app/controllers/foreman_kubevirt/concerns/api/compute_resources_controller_extensions.rb
@@ -2,17 +2,17 @@ module ForemanKubevirt
   module Concerns
     module Api
       module ComputeResourcesControllerExtensions
-          extend ::Apipie::DSL::Concern
-          update_api(:create, :update) do
-            param :compute_resource, Hash do
-              param :token, String, :desc => N_("Token for KubeVirt only")
-              param :hostname, String, :desc => N_("Host name for KubeVirt only")
-              param :namespace, String, :desc => N_("Namespace for KubeVirt only")
-              param :ca_crt, String, :desc => N_("CA crt for KubeVirt only")
-              param :api_port, String, :desc => N_("API port for KubeVirt only")
-            end
+        extend ::Apipie::DSL::Concern
+        update_api(:create, :update) do
+          param :compute_resource, Hash do
+            param :token, String, :desc => N_("Token for KubeVirt only")
+            param :hostname, String, :desc => N_("Host name for KubeVirt only")
+            param :namespace, String, :desc => N_("Namespace for KubeVirt only")
+            param :ca_crt, String, :desc => N_("CA crt for KubeVirt only")
+            param :api_port, String, :desc => N_("API port for KubeVirt only")
           end
         end
+      end
     end
   end
 end
--- app/validators/volume_validator.rb
+++ app/validators/volume_validator.rb
@@ -2,9 +2,9 @@ class VolumeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value[:volumes_attributes].present?
       value[:volumes_attributes].each do |_, attrs|
-        if attrs.key?("capacity") and attrs.key?("storage_class")
+        if attrs.key?("capacity") && attrs.key?("storage_class")
           if attrs["capacity"].to_s.empty? || /\A\d+G?\Z/.match(attrs["capacity"].to_s).nil?
-            record.errors.add(attribute, _("Volume size #{attrs["capacity"]} is not valid"))
+            record.errors.add(attribute, _("Volume size #{attrs['capacity']} is not valid"))
           end
         end
       end
